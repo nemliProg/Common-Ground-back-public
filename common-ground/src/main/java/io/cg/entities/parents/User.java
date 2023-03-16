a62a43af76c0af0d56d152c95d0634df2ee6ebd8
nemliProg <younesbuzz@gmail.com>
2023-03-16 09:51:29 +0100

    create the super class User

---
+++ common-ground/src/main/java/io/cg/entities/parents/User.java
@@ -0,0 +1,33 @@
+package io.cg.entities.parents;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+public class User {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+
+
+    @Column( unique = true , nullable = false)
+    private String username;
+
+    @Column( unique = true, nullable = false)
+    private String email;
+
+    @Column( nullable = false)
+    private String password;
+
+}
