c4b940dfa9ee0e8423251d877b0b1f0e180350fd
nemliProg <younesbuzz@gmail.com>
2023-03-16 21:54:34 +0100

    add method to find the agent member by email

--- common-ground/src/main/java/io/cg/repositories/MemberRepository.java
+++ common-ground/src/main/java/io/cg/repositories/MemberRepository.java
@@ -2,6 +2,7 @@ package io.cg.repositories;
 
 import io.cg.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -12,6 +13,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @RestResource(exported = false)
     Optional<Member> findByEmail(String email);
+
+    @RestResource(exported = false)
+    @Query(value = "SELECT m.* FROM member m WHERE m.email LIKE :email AND m.role = 1", nativeQuery = true)
+    Optional<Member> findAgentMemberByEmail(String email);
 
 
 }
