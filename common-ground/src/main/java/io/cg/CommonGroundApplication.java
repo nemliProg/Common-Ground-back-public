ce3525656de4d1a56cee56953d9b01972aeb25ca
nemliProg <younesbuzz@gmail.com>
2023-03-16 09:38:50 +0100

    initial commit : create the project

---
+++ common-ground/src/main/java/io/cg/CommonGroundApplication.java
@@ -0,0 +1,13 @@
+package io.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonGroundApplication.class, args);
+    }
+
+}
---
+++ common-ground/src/test/java/io/cg/CommonGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
