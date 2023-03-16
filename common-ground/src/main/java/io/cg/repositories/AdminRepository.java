b476e2cf705666943b5b8f30d2e4a590f8f9fc27
nemliProg <younesbuzz@gmail.com>
2023-03-16 16:35:58 +0100

    add findByEmail Method in Member and Admin repositories

--- common-ground/src/main/java/io/cg/repositories/AdminRepository.java
+++ common-ground/src/main/java/io/cg/repositories/AdminRepository.java
@@ -3,8 +3,16 @@ package io.cg.repositories;
 import io.cg.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Optional;
 
 
 @RepositoryRestResource
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    @RestResource(exported = false)
+    Optional<Admin> findByEmail(String email);
+
+
 }
--- common-ground/src/main/java/io/cg/repositories/MemberRepository.java
+++ common-ground/src/main/java/io/cg/repositories/MemberRepository.java
@@ -3,7 +3,15 @@ package io.cg.repositories;
 import io.cg.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Optional;
 
 @RepositoryRestResource
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @RestResource(exported = false)
+    Optional<Member> findByEmail(String email);
+
+
 }
