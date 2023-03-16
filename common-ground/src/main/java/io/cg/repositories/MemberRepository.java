e8aaeb0035e52eff55197ede01435ad96e54cfe6
nemliProg <younesbuzz@gmail.com>
2023-03-16 11:22:50 +0100

    add Member Repository

---
+++ common-ground/src/main/java/io/cg/repositories/MemberRepository.java
@@ -0,0 +1,9 @@
+package io.cg.repositories;
+
+import io.cg.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
