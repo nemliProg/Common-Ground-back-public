5aeb247088dbabfd6717cec803a21d401416e534
nemliProg <younesbuzz@gmail.com>
2023-03-16 22:30:21 +0100

    add empty contoller for each role

---
+++ common-ground/src/main/java/io/cg/api/AgentController.java
@@ -0,0 +1,12 @@
+package io.cg.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/agent")
+public class AgentController {
+
+
+}
