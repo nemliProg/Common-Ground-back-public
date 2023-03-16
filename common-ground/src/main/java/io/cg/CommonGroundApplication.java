183861b01786e9b6ba3f2efe073386d4f317d5e7
nemliProg <younesbuzz@gmail.com>
2023-03-16 20:25:45 +0100

    add BCryptPasswordEncoder Bean

--- common-ground/src/main/java/io/cg/CommonGroundApplication.java
+++ common-ground/src/main/java/io/cg/CommonGroundApplication.java
@@ -2,6 +2,9 @@ package io.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CommonGroundApplication {
@@ -10,4 +13,8 @@ public class CommonGroundApplication {
         SpringApplication.run(CommonGroundApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
